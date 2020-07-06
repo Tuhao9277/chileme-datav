dc08f5ab5f044a4c8d875228111386edc1800db1
gsh0581 <guo13145921@gmail.com>
2020-07-06 11:46:11 +0800

    first init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import './plugins/element';
+import './plugins/vcharts';
+import ECharts from 'echarts';
+import VueECharts from 'vue-echarts';
+import './style/index.css';
 
 Vue.config.productionTip = false;
+Vue.prototype.$echarts = ECharts;
+Vue.component('v-chart', VueECharts);
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <top-view />
+    <sales-view />
+    <bottom-view />
+    <map-view />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import TopView from '../components/TopView';
+import SalesView from '../components/SalesView';
+import BottomView from '../components/BottomView';
+import MapView from '../components/MapView';
+import { wordcloud, screenData, mapScatter } from '../api';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    TopView,
+    SalesView,
+    BottomView,
+    MapView,
+  },
+  data() {
+    return {
+      reportData: null,
+      wordCloud: null,
+      mapData: null,
+    };
+  },
+  methods: {
+    getReportData() {
+      return this.reportData;
+    },
+    getWordCloud() {
+      return this.wordCloud;
+    },
+    getMapData() {
+      return this.mapData;
+    },
+  },
+  provide() {
+    return {
+      getReportData: this.getReportData,
+      getWordCloud: this.getWordCloud,
+      getMapData: this.getMapData,
+    };
+  },
+  mounted() {
+    screenData().then(data => {
+      this.reportData = data;
+    });
+    wordcloud().then(data => {
+      this.wordCloud = data;
+    });
+    mapScatter().then(data => {
+      this.mapData = data;
+    });
   },
 };
 </script>
+
+<style>
+.home {
+  width: 100%;
+  padding: 20px;
+  background: #eee;
+  box-sizing: border-box;
+}
+</style>
